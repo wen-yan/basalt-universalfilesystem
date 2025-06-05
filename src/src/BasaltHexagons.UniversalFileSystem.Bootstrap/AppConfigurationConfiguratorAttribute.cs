wen-yan/basalthexagons-universalfilesystem

Implement cp command

src/src/BasaltHexagons.UniversalFileSystem.Cli/DependencyInjection/AppConfigurationConfiguratorAttribute.cs
@@ -1,0 +1,12 @@
+using System;
+
+namespace BasaltHexagons.UniversalFileSystem.Cli;
+
+/// <summary>
+/// This attribute is used to mark static method which is used to configure services.
+/// When `Profiles` is empty or it contains any profile, the marked method is invoked.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class AppConfigurationConfiguratorAttribute : Attribute
+{
+}

src/src/BasaltHexagons.UniversalFileSystem.Cli/DependencyInjection/ServicesConfiguratorAttribute.cs
@@ -1,0 +1,12 @@
+using System;
+
+namespace BasaltHexagons.UniversalFileSystem.Cli;
+
+/// <summary>
+/// This attribute is used to mark static method which is used to configure services.
+/// When `Profiles` is empty or it contains any profile, the marked method is invoked.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class ServicesConfiguratorAttribute : Attribute
+{
+}

src/src/BasaltHexagons.UniversalFileSystem.Cli/Commands/CatCommand.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using BasaltHexagons.CommandLine;
 using BasaltHexagons.CommandLine.Annotations;
+using BasaltHexagons.UniversalFileSystem.Cli.Commands.FileSystem;
 using BasaltHexagons.UniversalFileSystem.Cli.Output;
 
 namespace BasaltHexagons.UniversalFileSystem.Cli.Commands;

src/src/BasaltHexagons.UniversalFileSystem.Cli/Commands/RemoveCommand.cs
@@ -3,6 +3,7 @@ using System.CommandLine;
 using System.Threading.Tasks;
 using BasaltHexagons.CommandLine;
 using BasaltHexagons.CommandLine.Annotations;
+using BasaltHexagons.UniversalFileSystem.Cli.Commands.FileSystem;
 using BasaltHexagons.UniversalFileSystem.Cli.Output;
 
 namespace BasaltHexagons.UniversalFileSystem.Cli.Commands;

wen-yan/basalt-universalfilesystem

src/src/BasaltHexagons.UniversalFileSystem.Bootstrap/AppConfigurationConfiguratorAttribute.cs
@@ -1,0 +1,12 @@
+using System;
+
+namespace BasaltHexagons.UniversalFileSystem.Bootstrap;
+
+/// <summary>
+/// This attribute is used to mark static method which is used to configure services.
+/// When `Profiles` is empty or it contains any profile, the marked method is invoked.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class AppConfigurationConfiguratorAttribute : Attribute
+{
+}

src/src/BasaltHexagons.UniversalFileSystem.Bootstrap/ServicesConfiguratorAttribute.cs
@@ -1,0 +1,12 @@
+using System;
+
+namespace BasaltHexagons.UniversalFileSystem.Bootstrap;
+
+/// <summary>
+/// This attribute is used to mark static method which is used to configure services.
+/// When `Profiles` is empty or it contains any profile, the marked method is invoked.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class ServicesConfiguratorAttribute : Attribute
+{
+}

src/src/BasaltHexagons.UniversalFileSystem.Cli/Commands/FileSystem/CatCommand.cs
@@ -5,7 +5,7 @@ using BasaltHexagons.CommandLine;
 using BasaltHexagons.CommandLine.Annotations;
 using BasaltHexagons.UniversalFileSystem.Cli.Output;
 
-namespace BasaltHexagons.UniversalFileSystem.Cli.Commands;
+namespace BasaltHexagons.UniversalFileSystem.Cli.Commands.FileSystem;
 
 partial class CatCommandOptions
 {

src/src/BasaltHexagons.UniversalFileSystem.Cli/Commands/FileSystem/RemoveCommand.cs
@@ -5,7 +5,7 @@ using BasaltHexagons.CommandLine;
 using BasaltHexagons.CommandLine.Annotations;
 using BasaltHexagons.UniversalFileSystem.Cli.Output;
 
-namespace BasaltHexagons.UniversalFileSystem.Cli.Commands;
+namespace BasaltHexagons.UniversalFileSystem.Cli.Commands.FileSystem;
 
 partial class RemoveCommandOptions
 {